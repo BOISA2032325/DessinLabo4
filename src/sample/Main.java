b390378cfbd56d8ad6eb5a0db83cf9ad2efae8bb
sambp <2032325@etu.cegepjonquiere.ca>
2021-09-22 20:44:02 -0400

    commit 1

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -9,6 +9,7 @@ import javafx.scene.Scene;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.Line;
+import javafx.scene.shape.Polygon;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
@@ -20,22 +21,19 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
 
-        primaryStage.setWidth(700);
+        primaryStage.setWidth(1400);
         primaryStage.setHeight(500);
         primaryStage.setTitle("Maison");
-
-        //La maison
         Maison maison1 = new Maison();
-        Group maison= new Group();
+        Group dessin= new Group();
+        Rectangle deuxiemePartieDuDessin = new Rectangle(700,0,700,500);
+        deuxiemePartieDuDessin.setFill(Color.BLACK);
 
-        //Soleil
+
 
         Soleil soleil1 = new Soleil();
-
-        maison.getChildren().addAll(maison1.getMaisonFinal(),soleil1.getSoleilTotal());
-
-
-        primaryStage.setScene(new Scene(maison));
+        dessin.getChildren().addAll(deuxiemePartieDuDessin,maison1.getMaisonFinal(),soleil1.getSoleilTotal());
+        primaryStage.setScene(new Scene(dessin));
 
         primaryStage.getScene().setFill(Color.LIGHTGREY);
 
--- src/sample/Maison.java
+++ src/sample/Maison.java
@@ -13,6 +13,11 @@ public class Maison {
     Circle poigner = new Circle();
     Polygon triangle = new Polygon(245,175,350,100,455,175);
     Group fenetres = new Group();
+    Rectangle rectangle2 = new Rectangle();
+    Rectangle porte2 = new Rectangle();
+    Circle poigner2 = new Circle();
+    Polygon triangle2 = new Polygon(945,175,1050,100,1155,175);
+    Group fenetres2 = new Group();
     Group maisonFinal = new Group();
 
 
@@ -68,8 +73,60 @@ public class Maison {
         return triangle;
     }
 
+    public Group getFenetres2() {
+        Rectangle fenetre2 = new Rectangle();
+        fenetre2.setWidth(65);
+        fenetre2.setHeight(50);
+        fenetre2.setTranslateX(1065);
+        fenetre2.setTranslateY(210);
+        fenetre2.setStrokeWidth(5);
+        fenetre2.setStroke(Color.GRAY);
+        fenetre2.setFill(Color.LIGHTSKYBLUE);
+        Line ligne3 = new Line(1098,211,1098,259);
+        Line ligne4 = new Line(1065,236,1130,236);
+        ligne4.setStrokeWidth(5);
+        ligne4.setStroke(Color.GRAY);
+        ligne3.setStrokeWidth(5);
+        ligne3.setStroke(Color.GRAY);
+        ligne3.setFill(Color.GRAY);
+        fenetres2.getChildren().addAll(fenetre2,ligne3,ligne4);
+        return fenetres2;
+    }
+
+    public Circle getPoigner2() {
+        poigner2.setRadius(5);
+        poigner2.setTranslateX(1015);
+        poigner2.setTranslateY(295);
+        poigner2.setFill(Color.YELLOW);
+        return poigner2;
+    }
+
+    public Rectangle getRectangle2() {
+        rectangle2.setHeight(150);
+        rectangle2.setWidth(200);
+        rectangle2.setTranslateX(950);
+        rectangle2.setTranslateY(175);
+        rectangle2.setFill(Color.BEIGE);
+        return rectangle2;
+    }
+
+    public Rectangle getPorte2() {
+        porte2.setHeight(60);
+        porte2.setWidth(45);
+        porte2.setTranslateY(265);
+        porte2.setTranslateX(980);
+        porte2.setFill(Color.SADDLEBROWN);
+        return porte2;
+    }
+
+    public Polygon getTriangle2() {
+        triangle2.setFill(Color.BROWN);
+
+        return triangle2;
+    }
+
     public Group getMaisonFinal() {
-        maisonFinal.getChildren().addAll(getRectangle(),getPorte(),getPoigner(),getFenetres(),getTriangle());
+        maisonFinal.getChildren().addAll(getRectangle(),getPorte(),getPoigner(),getFenetres(),getTriangle(),getRectangle2(),getPorte2(),getPoigner2(),getFenetres2(),getTriangle2());
         return maisonFinal;
     }
 
